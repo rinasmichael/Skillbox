f18ab81c3485016582c0eb019e345caebdd81b12
rinasmichael <44485831+rinasmichael@users.noreply.github.com>
2025-06-21 18:30:15 +0200

    Add files via upload
    v1

---
+++ PKI/easy-rsa-final-0.1/debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/easy-rsa-final/issues
+# Bug-Submit: https://github.com/<user>/easy-rsa-final/issues/new
+# Changelog: https://github.com/<user>/easy-rsa-final/blob/master/CHANGES
+# Documentation: https://github.com/<user>/easy-rsa-final/wiki
+# Repository-Browse: https://github.com/<user>/easy-rsa-final
+# Repository: https://github.com/<user>/easy-rsa-final.git
