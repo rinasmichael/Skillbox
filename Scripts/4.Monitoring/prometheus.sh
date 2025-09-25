b2b30abb7d55f26472a4fd749e27caea06a5abaa
rinasmichael <44485831+rinasmichael@users.noreply.github.com>
2025-09-25 12:08:44 +0200

    Update prometheus.sh
    add graphana

--- Scripts/4.Monitoring/prometheus.sh
+++ Scripts/4.Monitoring/prometheus.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install prometheus prometheus-alertmanager -y
-
+sudo apt-get install -y adduser libfontconfig1 musl
+wget https://dl.grafana.com/grafana-enterprise/release/12.2.0/grafana-enterprise_12.2.0_17949786146_linux_amd64.deb
+sudo dpkg -i grafana-enterprise_12.2.0_17949786146_linux_amd64.deb
+sudo systemctl daemon-reload
+sudo systemctl restart grafana-server.service
+sudo systemctl enable grafana-server.service
+sudo iptables -A INPUT -p tcp --dport 3000 -j ACCEPT -m comment --comment grafana
 
 
 
